ce99a091f738d8eeae3ff4e199756fdad1ab8f6c
Dylan Trepos <dylan.trepos@gmail.com>
2023-08-31 20:12:45 +0200

    feat(chore): clean project

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header__container">
+    <div class="header__title">Poker planning</div>
+  </div>
+</template>
+
+<style scoped>
+.header__container {
+  height: 50px;
+}
+
+.header__title {
+  color: red;
+}
+</style>
