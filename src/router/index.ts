be364ae3cc61634510854914e2aede799e53a6d9
Dylan Trepos <dylan.trepos@gmail.com>
2023-10-11 22:27:58 +0200

    feat(settings): implement settings options

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ import useGeneralStore from '@/store/useGeneralStore';
 import useRoomStore from '@/store/useRoomStore';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_CLIENT_ADDRESS),
   routes: [
     {
       path: '/',
